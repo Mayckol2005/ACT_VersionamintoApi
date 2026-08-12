caf3278ac80e74ed5b858ca8dcc562c533fa0c14
00awakedead <abelcabero019@gmail.com>
2026-08-12 11:16:48 -0400

    Version 1.1.0: Creacion de nuevo endpoint

--- src/main/java/com/actividad/actVersiones/controller/ActController.java
+++ src/main/java/com/actividad/actVersiones/controller/ActController.java
@@ -1,6 +1,7 @@
 package com.actividad.actVersiones.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,11 @@ public class ActController {
     @GetMapping
     public String Saludo() {
         return "Hola Mundo";
+    }
+
+    @PostMapping
+    public String Despedida() {
+        return "Adíos Mundo";
     }
 
 }
